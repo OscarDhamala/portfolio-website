ec01fc1e1553ad7b1cac511b02835d35a61c58ec
OscarDhamala <oscardhamala117@gmail.com>
2023-05-23 16:27:10 +0545

    change commit

---
+++ oscar.js
@@ -0,0 +1,8 @@
+document.querySelector("a.hamicon").addEventListener("click", function(event){
+    document.getElementById("sideNavigation").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  });
+  document.querySelector("a.close-btn").addEventListener("click", function(event){
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  });
